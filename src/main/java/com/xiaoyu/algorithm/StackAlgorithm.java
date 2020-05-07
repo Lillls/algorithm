2e84a9b14c653a625001717a743be17c17f0e593
xiaoyu <lixiaoyu3@cmcm.com>
2020-05-07 14:20:49 +0800

    每日温度

--- src/main/java/com/xiaoyu/algorithm/StackAlgorithm.java
+++ src/main/java/com/xiaoyu/algorithm/StackAlgorithm.java
@@ -6,7 +6,15 @@ import java.util.Stack;
 public class StackAlgorithm {
 
     public static void main(String[] args) {
-        System.out.println(isValid("()[]{}{{{{}{}{}{234}}}}"));
+        int[] ints = dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
+        int[] ints2 = dailyTemperatures2(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+
+        for (int anInt : ints2) {
+            System.out.println(anInt);
+        }
     }
 
     /**
@@ -45,4 +53,49 @@ public class StackAlgorithm {
     /**
      * 大话数据结构P108页 中缀表达式变后缀表达式
      */
+
+    /**
+     * 每日温度
+     * 根据每日 气温 列表，请重新生成一个列表，对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+     * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+     * 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/daily-temperatures
+     * <p>
+     * 解题思路:
+     * 1.当索引为0:73,堆栈为空，把索引压入堆栈
+     * 2.当索引为1:74度大于73度,把73的下标度出栈,74的索引-73的索引,就是天数.
+     * 3.当索引为1入栈
+     * 4.同理索引2一样
+     * 5.当索引为3时:71小于75,只需要将索引3入栈
+     * 6.同理索引4一样
+     * 7.当索引为5时,72>69,将69出栈, 栈顶的71还是小于72,将71也出栈,直到栈顶大于72,或者栈为空,将72入栈
+     * ....
+     */
+    //巧妙的方法
+    private static int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] counts = new int[temperatures.length];
+        for (int i = 0; i < counts.length; i++) {
+            while (!stack.empty() && temperatures[i] > temperatures[stack.peek()]) {
+                Integer pop = stack.pop();
+                counts[pop] = i - pop;
+            }
+            stack.push(i);
+        }
+        return counts;
+    }
+    //笨方法
+    private static int[] dailyTemperatures2(int[] temperatures) {
+        int[] counts = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            for (int j = i + 1; j < temperatures.length; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    counts[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return counts;
+    }
 }
